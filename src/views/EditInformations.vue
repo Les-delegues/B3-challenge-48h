8d4e8460ed4815878906cf4111d43811cb12f6c9
Patrick Piquart <patrick.piquart@ynov.com>
2021-02-08 21:12:58 +0100

    J'ai des cravattes Van Gogh... Keith Haring tiens, Keith Haring le célèbre artiste. L'homme est un loup pour l'homme !

--- src/views/EditInformations.vue
+++ src/views/EditInformations.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-between items-center">
     <h1 class="text-2xl font-bold py-4 text-blue-brand">Informations de l'association CCBA</h1>
-    <router-link to="/" class="bg-gray-brand p-2 rounded-lg">
+    <router-link to="/informations" class="bg-gray-brand p-2 rounded-lg">
       <svg class="h-6 w-6 text-gray-dark" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
       </svg>
@@ -11,41 +11,50 @@
   <form method="POST" class="border border-gray-brand w-full rounded-lg shadow-md">
     <div class="flex flex-row px-4 border-b border-gray-light py-3">
       <label for="name" class="text-gray-dark w-1/4">Nom</label>
-      <input id="name" v-model="name" type="text" class="text-blue-brand w-2/4 bg-gray-brand px-3 py-2 rounded-full" />
+      <input id="name" v-model="name" type="text" class="input" />
     </div>
     <div class="flex flex-row px-4 border-b border-gray-light py-3">
       <label for="acronym" class="text-gray-dark w-1/4">Acronyme</label>
-      <input id="acronym" v-model="acronym" class="text-blue-brand w-2/4 bg-gray-brand px-3 py-2 rounded-full" />
+      <input id="acronym" v-model="acronym" class="input" />
     </div>
     <div class="flex flex-row px-4 border-b border-gray-light py-3">
       <label for="object" class="text-gray-dark w-1/4">Objet</label>
-      <input id="object" v-model="object" class="text-blue-brand w-2/4 bg-gray-brand px-3 py-2 rounded-full"/>
+      <input id="object" v-model="object" class="input"/>
     </div>
     <div class="flex flex-row px-4 border-b border-gray-light py-3">
       <label for="address1" class="text-gray-dark w-1/4">Adresse ligne 1</label>
-      <input id="address1" v-model="address1" class="text-blue-brand w-2/4 bg-gray-brand px-3 py-2 rounded-full"/>
+      <input id="address1" v-model="address1" class="input"/>
     </div>
     <div class="flex flex-row px-4 border-b border-gray-light py-3">
       <label for="address2" class="text-gray-dark w-1/4">Adresse ligne 2</label>
-      <input id="address2" v-model="address2" class="text-blue-brand w-2/4 bg-gray-brand px-3 py-2 rounded-full"/>
+      <input id="address2" v-model="address2" class="input"/>
     </div>
     <div class="flex flex-row px-4 border-b border-gray-light py-3">
       <label for="zipcode" class="text-gray-dark w-1/4">Code postal</label>
-      <input id="zipcode" v-model="zipcode" class="text-blue-brand w-2/4 bg-gray-brand px-3 py-2 rounded-full"/>
+      <input id="zipcode" v-model="zipcode" class="input"/>
     </div>
     <div class="flex flex-row px-4 border-b border-gray-light py-3">
       <label for="city" class="text-gray-dark w-1/4">Ville</label>
-      <input id="city" v-model="city" class="text-blue-brand w-2/4 bg-gray-brand px-3 py-2 rounded-full"/>
+      <input id="city" v-model="city" class="input"/>
     </div>
     <div class="flex flex-row px-4 border-b border-gray-light py-3">
       <label for="country" class="text-gray-dark w-1/4">Pays</label>
-      <input id="country" v-model="country" class="text-blue-brand w-2/4 bg-gray-brand px-3 py-2 rounded-full"/>
+      <input id="country" v-model="country" class="input"/>
     </div>
     <div class="flex flex-row px-4 py-3">
       <label for="creationDate" class="text-gray-dark w-1/4">Date de création</label>
-      <input id="creationDate" v-model="creationDate" class="text-blue-brand w-2/4 bg-gray-brand px-3 py-2 rounded-full"/>
+      <input type="date" id="creationDate" v-model="creationDate" class="input"/>
     </div>
   </form>
+
+  <div class="flex flex-row-reverse">
+    <button
+      type="submit"
+      class="button-primary"
+    >
+      Sauvegarder
+    </button>
+  </div>
 </template>
 
 <script>
