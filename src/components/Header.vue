fe4fefa90089ff22b88c39a7b0c29f9c8a5bc85d
Ovid <ovid@c19.org>
2021-02-08 16:47:21 +0100

    The cause is hidden. The effect is visible to all.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,12 +4,12 @@
         <div class="relative flex justify-between xl:grid xl:grid-cols-12 lg:gap-8">
           <div class="flex md:absolute md:left-0 mdc:inset-y-0 lg:static xl:col-span-2">
             <div class="flex-shrink-0 flex items-center">
-              <a href="/">
+              <router-link to="/">
                 <div class="flex inline-flex items-center">
                 <img class="block h-12 w-12" src="../assets/logo.png" alt="CCBA">
-                  <h1 class="pl-4 font-bold">CCBA - HelloPlatform</h1>
+                  <h1 class="pl-4 font-bold text-blue-brand">CCBA - HelloPlatform</h1>
                 </div>
-              </a>
+              </router-link>
             </div>
           </div>
 
