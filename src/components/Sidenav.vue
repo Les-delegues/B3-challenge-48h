2e248337468a399a52950fb346794bd73cf193b9
Ovid <ovid@c19.org>
2021-02-08 16:47:21 +0100

    The cause is hidden. The effect is visible to all.

--- src/components/Sidenav.vue
+++ src/components/Sidenav.vue
@@ -1,23 +1,31 @@
 <template>
   <nav class="bg-blue-brand w-56 fixed flex flex-col min-h-screen text-white py-4 px-4">
-    <section class="opacity-80">
+    <section>
       <ul>
         <li>
-          <router-link to="/" exact active-class="opacity-100 font-bold">Tableau de bord</router-link>
+          <router-link to="/" exact class="opacity-80" active-class="opacity-100 font-bold">Tableau de bord</router-link>
         </li>
       </ul>
-      <h4 class="uppercase mt-3">Association</h4>
+      <h4 class="uppercase mt-3 opacity-80">Association</h4>
       <ul>
         <li class="px-4">
-          <router-link to="/informations" exact active-class="opacity-100 font-bold">Informations</router-link>
+          <router-link to="/informations" exact class="opacity-80" active-class="opacity-100 font-bold">Informations</router-link>
         </li>
-        <li class="px-4">Documents</li>
-        <li class="px-4">Calendrier des aides</li>
+        <li class="px-4">
+          <router-link to="/documents" exact class="opacity-80" active-class="opacity-100 font-bold">Documents</router-link>
+        </li>
+        <li class="px-4">
+          <router-link to="/calendrier" exact class="opacity-80" active-class="opacity-100 font-bold">Calendrier des aides</router-link>
+        </li>
       </ul>
-      <h4 class="uppercase mt-3">Mes modules</h4>
+      <h4 class="uppercase mt-3 opacity-80">Mes modules</h4>
       <ul>
-        <li class="px-4">Les licenciés</li>
-        <li class="px-4">Les matchs</li>
+        <li class="px-4">
+          <router-link to="/licencies" exact class="opacity-80" active-class="opacity-100 font-bold">Les licenciés</router-link>
+        </li>
+        <li class="px-4">
+          <router-link to="/matchs" exact class="opacity-80" active-class="opacity-100 font-bold">Les matchs</router-link>
+        </li>
       </ul>
     </section>
   </nav>
