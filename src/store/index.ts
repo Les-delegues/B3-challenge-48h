5bc85381326e75369db984634b3b9a48482cd683
Benjamin Franklin <benjamin.franklin@politics.gov>
2021-02-08 13:39:08 +0100

    Lost time is never found again.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,8 @@
 import { createStore } from 'vuex';
+import { calendarInfoModule } from './modules';
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    calendarInfoModule,
   },
 });
