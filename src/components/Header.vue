4d06292817a252b4bdced35abb1009b10eb13416
Kent Beck <kent.beck@xtrm.com>
2021-02-08 15:36:26 +0100

    Make it work, make it right, make it fast.

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="bg-white shadow lg:static lg:overflow-y-visible">
+      <div class="max-w-7xl px-4 py-3">
+        <div class="relative flex justify-between xl:grid xl:grid-cols-12 lg:gap-8">
+          <div class="flex md:absolute md:left-0 mdc:inset-y-0 lg:static xl:col-span-2">
+            <div class="flex-shrink-0 flex items-center">
+              <a href="/">
+                <div class="flex inline-flex items-center">
+                <img class="block h-12 w-12" src="../assets/logo.png" alt="CCBA">
+                  <h1 class="pl-4 font-bold">CCBA - HelloPlatform</h1>
+                </div>
+              </a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
---
+++ src/components/Sidenav.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="bg-blue-brand w-56 fixed flex flex-col min-h-screen text-white py-4 px-4">
+    <section class="opacity-80">
+      <ul>
+        <li>
+          <router-link to="/" exact active-class="opacity-100 font-bold">Tableau de bord</router-link>
+        </li>
+      </ul>
+      <h4 class="uppercase mt-3">Association</h4>
+      <ul>
+        <li class="px-4">Informations</li>
+        <li class="px-4">Documents</li>
+        <li class="px-4">Calendrier des aides</li>
+      </ul>
+      <h4 class="uppercase mt-3">Mes modules</h4>
+      <ul>
+        <li class="px-4">Les licenciés</li>
+        <li class="px-4">Les matchs</li>
+      </ul>
+    </section>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Sidenav',
+};
+</script>
