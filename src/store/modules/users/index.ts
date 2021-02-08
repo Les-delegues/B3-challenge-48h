053b2d9de74010553a1f13caabb22bc21b4eb3e2
Alexandre Dubois <contact@alexandredubois.com>
2021-02-08 22:01:55 +0100

    PIQUART Patrick proposait 3 minutes mais je lui ai rappelé qu'on avait DIAS Steven

---
+++ src/store/modules/users/index.ts
@@ -0,0 +1,15 @@
+import { Module } from 'vuex';
+import { RootState } from '@/store/root-state';
+import { State, state } from './state';
+import { actions } from './actions';
+import { getters } from './getters';
+import { mutations } from './mutations';
+
+const store: Module<State, RootState> = {
+  state,
+  getters,
+  mutations,
+  actions,
+};
+
+export default store;
