c664e2c57d267ef411dc4ab0af00380792ed774c
Donald Trump <donald.trump@dumb.tv>
2021-02-08 15:55:44 +0100

    This commit is fake news.

--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -1,1 +1,7 @@
+<<<<<<< Updated upstream
 export * from './calendarInfo';
+=======
+export * from './calendar-info.module';
+export * from './association-info.module';
+export * from './license.module';
+>>>>>>> Stashed changes
